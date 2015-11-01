024b037234a63b05fc765df30b3a4da3bba80426
Pierre-Antoine ROBERT <pierreantoine.robert@gmail.com>
2015-11-01 10:39:59 +0100

    elpa

---
+++ site-lisp/python-environment-20150310.153/python-environment-autoloads.el
@@ -0,0 +1,18 @@
+;;; python-environment-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("python-environment-pkg.el" "python-environment.el"
+;;;;;;  "test-python-environment.el") (22028 58731 571737 905000))
+
+;;;***
+
+(provide 'python-environment-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; python-environment-autoloads.el ends here
---
+++ site-lisp/s-20150924.406/s-autoloads.el
@@ -0,0 +1,15 @@
+;;; s-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("s.el") (22056 4094 131958 718000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; s-autoloads.el ends here
